5a9576a939b10cd519f7480e014e2362c9385928
RukiWang <benson16868@gmail.com>
2024-11-27 11:58:46 +0800

    week12

---
+++ week12/week12-4.cpp
@@ -0,0 +1,17 @@
+//week12-4.cpp not ok,not finished
+#include <iostream>
+using namespace std;
+int main(){
+	int T, N,now;
+	char c,c2; // not important!!
+	cin >> T; //step01:Input
+	for(int t=1;t<=T;t++){ //Test Case t
+		cin>>c>>c2>>N;	//step01: Input
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N;j++){
+				cin>>now; //step03: for-loop Input
+			}
+		}
+		cout<< "Test #"<< t<<": Symmetric.\n";
+	}//step02: Ouput
+}
